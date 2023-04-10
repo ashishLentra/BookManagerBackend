070f9ffea8adc02d6699e930ccc72efb807caaa1
ashishLentra <ashishya@lentra.ai>
2023-04-10 15:44:36 +0530

    Added user Repo

---
+++ src/main/java/com/bookManager/book_manager/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.bookManager.book_manager.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bookManager.book_manager.model.User;
+import com.bookManager.book_manager.repo.UserRepo;
+
+@RestController
+@CrossOrigin(origins = "*")
+public class UserController {
+	
+	@Autowired
+	private UserRepo repo;
+	
+	@GetMapping("/getUsers")
+	public List<User> getUser() {
+		return repo.findAll();
+	}
+	
+	
+	@PostMapping("/addUser")
+	public User addUser(@RequestBody User user) {
+		return repo.save(user);
+	}
+	
+	@PutMapping("/updateUser/{id}")
+	public User updateUser(@RequestBody User user) {
+		return repo.save(user);
+	}
+
+	@DeleteMapping("deleteUser/{id}")
+	public void deleteUser(@PathVariable int id) {
+		repo.deleteById(id);
+	}
+	
+}
---
+++ src/main/java/com/bookManager/book_manager/repo/UserRepo.java
@@ -0,0 +1,13 @@
+package com.bookManager.book_manager.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bookManager.book_manager.model.User;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer>{
+		
+		
+	
+}
